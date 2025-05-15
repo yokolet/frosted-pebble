a3fec8a8781fd4121d53954e4c25d0f671f765c9
Yoko Harada <yokolet@gmail.com>
2025-05-15 17:24:31 +0900

    updates the accordion to keep open if opened

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,31 +20,69 @@ function algoNavToggle() {
   algo_content.classList.toggle('hidden')
 }
 
+// accordion: SVG for Minus icon
+const minusSVG = `
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
+    <path d="M3.75 7.25a.75.75 0 0 0 0 1.5h8.5a.75.75 0 0 0 0-1.5h-8.5Z" />
+  </svg>
+`
+
+// accordion: SVG for Plus icon
+const plusSVG = `
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
+    <path d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z" />
+  </svg>
+`
+
+const getAccordionStatus = () => {
+  const items = sessionStorage.getItem('accordion-statuses')
+  if (items) {
+    return JSON.parse(items)
+  } else {
+    return {}
+  }
+}
+
+const setAccordionStatus = (index, status) => {
+  const items = sessionStorage.getItem('accordion-statuses')
+  let statuses
+  if (items) {
+    statuses = JSON.parse(items)
+  } else {
+    statuses = {}
+  }
+  statuses[index] = status
+  sessionStorage.setItem('accordion-statuses', JSON.stringify(statuses))
+}
+
+window.onload = () => {
+  const accordions = getAccordionStatus()
+  for (let index in accordions) {
+    let content = document.getElementById(`algo-content-${index}`)
+    let icon = document.getElementById(`algo-icon-${index}`)
+    if (accordions[index]) {
+      content.style.maxHeight = content.scrollHeight + 'px'
+      icon.innerHTML = minusSVG
+    } else {
+      content.style.maxHeight = '0'
+      icon.innerHTML = plusSVG
+    }
+  }
+}
+
 // accordion
 function toggleAccordion(index) {
-  const content = document.getElementById(`algo-content-${index}`);
-  const icon = document.getElementById(`algo-icon-${index}`);
-
-  // SVG for Minus icon
-  const minusSVG = `
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
-        <path d="M3.75 7.25a.75.75 0 0 0 0 1.5h8.5a.75.75 0 0 0 0-1.5h-8.5Z" />
-      </svg>
-    `;
-
-  // SVG for Plus icon
-  const plusSVG = `
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
-        <path d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z" />
-      </svg>
-    `;
+  const content = document.getElementById(`algo-content-${index}`)
+  const icon = document.getElementById(`algo-icon-${index}`)
 
   // Toggle the content's max-height for smooth opening and closing
   if (content.style.maxHeight && content.style.maxHeight !== '0px') {
-    content.style.maxHeight = '0';
-    icon.innerHTML = plusSVG;
+    content.style.maxHeight = '0'
+    icon.innerHTML = plusSVG
+    setAccordionStatus(index, false)
   } else {
-    content.style.maxHeight = content.scrollHeight + 'px';
-    icon.innerHTML = minusSVG;
+    content.style.maxHeight = content.scrollHeight + 'px'
+    icon.innerHTML = minusSVG
+    setAccordionStatus(index, true)
   }
 }
